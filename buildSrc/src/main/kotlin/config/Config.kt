7c68bd286c686176196a877cfb8e39e912b095c8
ldiego <lfdiego7@gmail.com>
2018-10-23 11:33:59 -0500

    Se finaliza las dependencias

---
+++ buildSrc/src/main/kotlin/config/Config.kt
@@ -0,0 +1,13 @@
+package config
+
+object Config {
+    object Android {
+        val minSdkVersion = 21
+        val compileSdkVersion = 28
+        val targetSdkVersion = 28
+        val applicationId = "com.ldiego.app"
+        val versionCode = 1
+        val versionName = "1.0"
+    }
+
+}
--- settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-rootProject.buildFileName = "build.gradle.bak.kts"
-
-include(":app")
