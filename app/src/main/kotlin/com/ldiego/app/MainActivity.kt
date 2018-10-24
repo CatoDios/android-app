848a1849de038c670c7c488941d337e585993ff9
Luis Diego <lfdiego7@gmail.com>
2018-10-24 00:18:40 -0500

    ADD Screen

--- app/src/main/kotlin/com/ldiego/app/MainActivity.kt
+++ app/src/main/kotlin/com/ldiego/app/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.ldiego.app
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        showFullScreen()
         setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+    private fun showFullScreen() {
+        val decorView = window.decorView
+        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_IMMERSIVE)
+    }
+
+    fun init() {
+        val pi = applicationContext.packageManager.getPackageInfo(packageName, 0)
+
+        txtVersion.text = pi.versionName
     }
 }
