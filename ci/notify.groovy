862fd07a6c6f458d6b08ad65e0207206a1fae6f7
ldiego <lfdiego7@gmail.com>
2018-10-25 14:00:37 -0500

    FIX

--- ci/notify.groovy
+++ ci/notify.groovy
@@ -12,9 +12,7 @@ def send(String buildStatus = "STARTED") {
     slackSend (
         channel: "#ci",
         color: code,
-        message: "BUILD $buildStatus: ${env.JOB_NAME} [${env.BUILD_NUMBER}]",
-        channel: "#ci",
-        tokenCredentialId: "k5Fu4i2e2Wky4MYMAfef5zSO"
+        message: "BUILD $buildStatus: ${env.JOB_NAME} [${env.BUILD_NUMBER}]"
     )
 }
 
